4674a9c5a7bfa8f409ecec75f0b168744f628d4b
iceEiji <guitar.pg.sc@gmail.com>
2021-09-23 23:15:25 +0900

    引数をStructOptで定義するように修正

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,24 @@
+/* 
+ * 概要:
+ * grep するコマンド
+ *
+ * 補足:
+ * 1. StructOptの機能により、構造体のドキュメントコメントからコマンドのhelpが生成される。
+ */
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    // コマンドライン引数の取得
+    let args = CommandLineInterface::from_args();
+}
+
+
+/// 指定したファイルから、特定の文字列を検索する。
+#[derive(StructOpt)]
+struct CommandLineInterface { 
+    /// 探したい文字列
+    pattern: String,
+    /// 検索したいファイルのパス
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
