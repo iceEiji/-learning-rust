d3402d32083c27d71f452f89664b997fce3a2d40
iceEiji <guitar.pg.sc@gmail.com>
2021-09-24 00:22:31 +0900

    ファイルの取得に対応

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -10,8 +10,20 @@ use structopt::StructOpt;
 fn main() {
     // コマンドライン引数の取得
     let args = CommandLineInterface::from_args();
-}
 
+    // ファイルの取得
+    // TODO BufReaderへの置き換え(https://doc.rust-lang.org/1.39.0/std/io/struct.BufReader.html)
+    let content = std::fs::read_to_string(&args.path)
+    .expect("could not read file");
+
+    // 各行の取得
+    for line in content.lines() {
+        // パターン（検索文字列）が含まれるか
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
 
 /// 指定したファイルから、特定の文字列を検索する。
 #[derive(StructOpt)]
