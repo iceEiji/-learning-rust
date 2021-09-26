5151ad2941583ed1c437242191a5954b25af2159
iceEiji <guitar.pg.sc@gmail.com>
2021-09-26 23:29:02 +0900

    検索結果表示の出力の性能改善。

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -8,6 +8,7 @@
  */
 use structopt::StructOpt;
 use anyhow::{Context, Result};
+use std::io::{self, Write};
 
 fn main() -> Result<()> {
     // コマンドライン引数の取得
@@ -17,23 +18,39 @@ fn main() -> Result<()> {
      * ファイルの取得
      *
      * TODO BufReaderへの置き換え(https://doc.rust-lang.org/1.39.0/std/io/struct.BufReader.html)
+     *
+     * 補足:
+     * 1. anyhow を使ってエラー出力が実装されている
+     * 下記は原始的（？）なエラー出力の実装。
+     * let result = std::fs::read_to_string(&args.path);
+     * let content = match result {
+     *     Ok(content) => { content },
+     *     Err(error) => { panic!("Can't deal with {}, just exit here.", error); }
+     * };
+     *
+     * 2. fmtについて
+     * {}は、Deisplayを出力するが、
+     * {:?}は、Debug（[derive(Debug)]）を出力する。
+     * fmtモジュール参照 https://doc.rust-lang.org/1.39.0/std/fmt/index.html
      */
-    // もっとも原始的なエラーハンドリングの実装。
-    // let result = std::fs::read_to_string(&args.path);
-    // let content = match result {
-    //     Ok(content) => { content },
-    //     Err(error) => { panic!("Can't deal with {}, just exit here.", error); }
-    // };
-
-    // anyhow を使った場合のエラーハンドリングの実装。
     let content = std::fs::read_to_string(&args.path)
     .with_context(|| format!("could not read file {:?}", &args.path))?;
 
-    // 各行の取得
+    /*
+     * 各行の取得・比較・マッチするものを表示。
+     *
+     * 補足：
+     * 1. printlnはループなどで呼び出すと遅い。
+     * ・毎回ロックされるから→直接stdoutを取得してロックの回数を１度だけに減らす
+     * ・毎回flushされるから→BufWriterを利用してバッファリングし回数を減らす
+     */
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout.lock());
     for line in content.lines() {
         // パターン（検索文字列）が含まれるか
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            writeln!(handle, "{}", line)
+            .with_context(|| "Couldn't write to stdout.")?;
         }
     }
 
