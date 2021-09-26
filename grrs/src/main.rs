828d49d020b7049824adc4e4d211f4de4c25728c
iceEiji <guitar.pg.sc@gmail.com>
2021-09-26 22:07:16 +0900

    エラー処理を改良

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -4,17 +4,30 @@
  *
  * 補足:
  * 1. StructOptの機能により、構造体のドキュメントコメントからコマンドのhelpが生成される。
+ * 2. anyhowの機能により、エラーハンドリング・メッセージを簡潔に。
  */
 use structopt::StructOpt;
+use anyhow::{Context, Result};
 
-fn main() {
+fn main() -> Result<()> {
     // コマンドライン引数の取得
     let args = CommandLineInterface::from_args();
 
-    // ファイルの取得
-    // TODO BufReaderへの置き換え(https://doc.rust-lang.org/1.39.0/std/io/struct.BufReader.html)
+    /*
+     * ファイルの取得
+     *
+     * TODO BufReaderへの置き換え(https://doc.rust-lang.org/1.39.0/std/io/struct.BufReader.html)
+     */
+    // もっとも原始的なエラーハンドリングの実装。
+    // let result = std::fs::read_to_string(&args.path);
+    // let content = match result {
+    //     Ok(content) => { content },
+    //     Err(error) => { panic!("Can't deal with {}, just exit here.", error); }
+    // };
+
+    // anyhow を使った場合のエラーハンドリングの実装。
     let content = std::fs::read_to_string(&args.path)
-    .expect("could not read file");
+    .with_context(|| format!("could not read file {:?}", &args.path))?;
 
     // 各行の取得
     for line in content.lines() {
@@ -23,6 +36,9 @@ fn main() {
             println!("{}", line);
         }
     }
+
+    // 正常終了
+    Ok(())
 }
 
 /// 指定したファイルから、特定の文字列を検索する。
